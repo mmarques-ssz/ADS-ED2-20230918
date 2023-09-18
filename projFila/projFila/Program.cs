81d01ebcb040031afd568f9a9515d0cad4e1f0c2
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2023-09-18 20:19:58 -0300

    Add files via upload

---
+++ projFila/projFila/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projFila
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> filaNum = new Queue<int>();
+
+            filaNum.Enqueue(6);
+            filaNum.Enqueue(12);
+            filaNum.Enqueue(9);
+            filaNum.Enqueue(5);
+            mostraFila(filaNum);
+
+            Console.WriteLine("Primeiro da fila: {0}", filaNum.Peek());
+            Console.WriteLine("Primeiro da fila: {0}", filaNum.First());
+            Console.WriteLine("Último da fila: {0}", filaNum.Last());
+            Console.WriteLine("Elemento na 2ª posição: {0}", filaNum.ElementAt(1));
+            Console.WriteLine("Desenfileirou: {0}", filaNum.Dequeue());
+            mostraFila(filaNum);
+
+            Console.WriteLine("Primeiro da fila: {0}", filaNum.Peek());
+            Console.WriteLine("Primeiro da fila: {0}", filaNum.First());
+            Console.WriteLine("Último da fila: {0}", filaNum.Last());
+            Console.WriteLine("Elemento na 2ª posição: {0}", filaNum.ElementAt(1));
+            Console.WriteLine("Desenfileirou: {0}", filaNum.Dequeue());
+            mostraFila(filaNum);
+        }
+
+        static void mostraFila(Queue<int> fn)
+        {
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Qtde de elementos na fila: {0}", fn.Count);
+            Console.WriteLine("---------------------------------");
+            foreach (int num in fn)
+            {
+                Console.WriteLine("Elemento: {0}", num);
+            }
+            Console.WriteLine("---------------------------------");
+        }
+
+
+    }
+}
